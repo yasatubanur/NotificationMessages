6a00f294120363239cba8da218ad50fc44197b83
Tuba Nur <yasatubanur@gmail.com>
2022-03-23 09:57:48 +0300

    notification properties edited

--- NotificationMessages/ViewController.swift
+++ NotificationMessages/ViewController.swift
@@ -21,13 +21,25 @@ class ViewController: UIViewController {
 
     
     @IBAction func signUpClicked(_ sender: Any) {
-        let notification = UIAlertController(title: "Error", message: "Please try again", preferredStyle: .alert)
-        
-        let okButton = UIAlertAction(title: "OK", style: .default) { UIAlertAction in
+        if emailTextField?.text == ""{
+            notification(title: "Email Error", message: "Please enter an email!")
+        }else if passwordTextField?.text == ""{
+            notification(title: "Password Error", message: "Please enter password!")
+        }else if password2TextField?.text == ""{
+            notification(title: "Password Error", message: "Please enter password again")
+        }else if passwordTextField?.text != password2TextField?.text{
+            notification(title: "Password Eror", message: "Please check passwords")
+        }else{
+            notification(title: "Done", message: "Saved!")
         }
+    }
+    
+    func notification(title: String, message: String){
+        let notification = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        notification.addAction(okButton)
-        
+        let okeyButton = UIAlertAction(title: "OK", style: .default) { UIAlertAction in
+        }
+        notification.addAction(okeyButton)
         self.present(notification, animated: true, completion: nil)
     }
     
